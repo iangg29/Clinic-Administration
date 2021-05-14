b0c15506bbbbf92c80beb8d6c3d11e12de949370
Ian García <iangarciag@outlook.com>
2021-05-14 10:02:37 -0500

    Added comment to all appointment related classes.

--- appointments/Appointment.h
+++ appointments/Appointment.h
@@ -16,6 +16,12 @@
 #include <iostream>
 #include <string>
 
+/*
+ * Appointment: Clase principal de cada consulta.
+ * @file Appointment.h
+ * @author Ian
+ * */
+
 class Appointment {
 private:
     int ID;
@@ -56,62 +62,117 @@ public:
     void setActive(bool active);
 };
 
+/**
+ * Constructor principal de la clase con parámetros.
+ * @param day Día de la consulta.
+ * @param month Mes de la consulta.
+ * @param cost Costo de la consulta.
+ * */
 Appointment::Appointment(int day, string month, int cost) {
     this->ID = 0;
     this->day = day;
     this->month = month;
     this->cost = cost;
-    this->active = true;
+    this->active = false;
 }
 
+/**
+ * Método para reservar la consulta.
+ * */
 void Appointment::reserve() {
 
 }
 
+/**
+ * Método para cancelar la consulta.
+ * */
 void Appointment::cancel() {
 
 }
 
+/**
+ * Método para pagar la consulta.
+ * */
 void Appointment::pay() {
     // TODO: Transaction
 }
 
+/**
+ * Obtiene el ID de la consulta.
+ * @return ID ID
+ * */
 int Appointment::getID() {
     return this->ID;
 }
 
+/**
+ * Asigna el ID a la consulta.
+ * @param ID
+ * */
 void Appointment::setID(int ID) {
     this->ID = ID;
 }
 
+/**
+ * Obtiene el día de la consulta.
+ * @return Día
+ * */
 int Appointment::getDay() {
     return this->day;
 }
 
+/**
+ * Asigna el día a la consulta.
+ * @param day Día
+ * */
 void Appointment::setDay(int day) {
     this->day = day;
 }
 
+/**
+ * Obtiene el mes de la consulta.
+ * @return Mes
+ * */
 string Appointment::getMonth() {
     return this->month;
 }
 
+/**
+ * Asigna el mes a la consulta.
+ * @param month Mes
+ * */
 void Appointment::setMonth(string month) {
     this->month = month;
 }
 
+/**
+ * Obtiene el costo de la consulta.
+ * @return Costo
+ * */
 int Appointment::getCost() {
     return this->cost;
 }
 
+/**
+ * Asignar el costo de la consulta.
+ * @param cost Costo
+ * */
 void Appointment::setCost(int cost) {
     this->cost = cost;
 }
 
+/**
+ * Verifica si la consulta está activa.
+ * @return Activa
+ * */
 bool Appointment::isActive() {
     return this->active;
 }
 
+/**
+ * Asignar si la consulta es activa.
+ * @param active Activas
+ * */
 void Appointment::setActive(bool active) {
     this->active = active;
 }
--- appointments/AppointmentManager.h
+++ appointments/AppointmentManager.h
@@ -19,6 +19,12 @@
 #include "../Module.h"
 #include "Appointment.h"
 
+/*
+ * AppointmentManager: Módulo que administra las consultas de la aplicación.
+ * @file AppointmentManager.h
+ * @author Ian
+ * */
+
 const string NAME_APPOINTMENT = "Appointment Manager";
 
 class AppointmentManager : public Module {
@@ -43,8 +49,15 @@ public:
     void logAppointment(Appointment &appointment);
 };
 
+/**
+ * Constructor de la clase sin parámetros.
+ * */
 AppointmentManager::AppointmentManager() : Module(NAME_APPOINTMENT) {}
 
+/**
+ * Consturctor princiapal de la clase con parámetros.
+ * @param app Instancia de la aplicación principal
+ * */
 AppointmentManager::AppointmentManager(Application *app) : Module(app, NAME_APPOINTMENT) {
     try {
         start();
@@ -53,27 +66,49 @@ AppointmentManager::AppointmentManager(Application *app) : Module(app, NAME_APPO
     }
 }
 
+/**
+ * Método inicial del módulo.
+ * */
 void AppointmentManager::start() {
     Module::start();
 }
 
+/**
+ * Método para finalización de módulo.
+ * */
 void AppointmentManager::end() {
     Module::end();
     log("CLEARING CACHE!");
 }
 
+/**
+ * Obtiene el mapa de consultas registradas en la aplicación.
+ * @return Consultas
+ * */
 unordered_map<int, Appointment> AppointmentManager::getAppointments() {
     return this->appointments;
 }
 
+/**
+ * Obtiene la cuenta de consultas registradas (aka ID).
+ * @return Cuenta
+ * */
 int AppointmentManager::getCount() {
     return this->count;
 }
 
+/**
+ * Asigna la cuenta al registro de consultas.
+ * @param count Nueva cuenta
+ * */
 void AppointmentManager::setCount(int count) {
     this->count = count;
 }
 
+/**
+ * Registro de una nueva consulta.
+ * @param appointment Objeto de la consulta
+ * */
 void AppointmentManager::logAppointment(Appointment &appointment) {
     int newID = getCount() + 1;
     appointment.setID(newID);
