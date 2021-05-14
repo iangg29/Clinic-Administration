9128ce47b5e1731e2f0618ccf700dee09e59a059
Ian García <iangarciag@outlook.com>
2021-05-14 10:31:23 -0500

    Added file comments to accounting

--- accounting/AccountingManager.h
+++ accounting/AccountingManager.h
@@ -18,6 +18,12 @@
 #include "../Module.h"
 #include "Transaction.h"
 
+/*
+ * AccountingManager: Clase de módulo que administra el balance de la clínica.
+ * @file AccountingManager.h
+ * @author Ian
+ * */
+
 const string NAME_ACCOUNTING = "Accounting Manager";
 
 class AccountingManager : public Module {
@@ -39,8 +45,15 @@ public:
     void addTransaction(Transaction transaction);
 };
 
+/**
+ * Constructor de la clase sin parámetros.
+ * */
 AccountingManager::AccountingManager() : Module(NAME_ACCOUNTING) {}
 
+/**
+ * Constructor principal de la clase con parámetros.
+ * @param app Instancia de la aplicación principal.
+ * */
 AccountingManager::AccountingManager(Application *app) : Module(app, NAME_ACCOUNTING) {
     try {
         start();
@@ -49,23 +62,41 @@ AccountingManager::AccountingManager(Application *app) : Module(app, NAME_ACCOUN
     }
 }
 
+/**
+ * Método de inicio del módulo.
+ * */
 void AccountingManager::start() {
     Module::start();
 }
 
+/**
+ * Método de finalización de módulo.
+ * */
 void AccountingManager::end() {
     Module::end();
     log("CLEARING CACHE!");
 }
 
+/**
+ * Obtiene el balance general de la clínica.
+ * @return Balance
+ * */
 float AccountingManager::getBalance() {
     return this->balance;
 }
 
+/**
+ * Asigna el balance general.
+ * @param balance Nuevo balance
+ * */
 float AccountingManager::setBalance(float balance) {
     this->balance = balance;
 }
 
+/**
+ * Agrega una transacción al historial.
+ * @param transaction Transacción
+ * */
 void AccountingManager::addTransaction(Transaction transaction) {
     // TODO: Transaction logic.
 }
--- accounting/Transaction.h
+++ accounting/Transaction.h
@@ -16,8 +16,20 @@
 #include <iostream>
 #include <string>
 
+/*
+ * Transaction: Clase principal de la transacción de la clínica.
+ * TransactionType: Tipo de transacción.
+ * @file Transaction.h
+ * @author Ian
+ * */
+
 using namespace std;
 
+/**
+ * Tipos de transacción disponible.
+ * INCOME: Ingreso
+ * OUTCOME: Gasto
+ * */
 enum class TransactionType {
     INCOME,
     OUTCOME
@@ -29,7 +41,7 @@ private:
     string reason;
     TransactionType type;
 public:
-    Transaction(int value, string reason, TransactionType type);
+    Transaction(float value, string reason, TransactionType type);
 
     float getValue();
 
@@ -44,32 +56,61 @@ public:
     void setType(TransactionType type);
 };
 
+/**
+ * Constructor principal de la clase con parámetros.
+ * @param value Valor de la transacción
+ * @param reason Razón de la transacción
+ * @param type Tipo de la transacción
+ * */
 Transaction::Transaction(float value, string reason, TransactionType type) {
     this->value = value;
     this->reason = reason;
     this->type = type;
 }
 
+/**
+ * Obtiene el valor de la transacción.
+ * */
 float Transaction::getValue() {
     return this->value;
 }
 
+/**
+ * Asigna el valor a la transacción.
+ * @param value Valor
+ * */
 void Transaction::setValue(float value) {
     this->value = value;
 }
 
+/**
+ * Obtiene la razón de la transacción.
+ * @return reason
+ * */
 string Transaction::getReason() {
     return this->reason;
 }
 
+/**
+ * Asigna la razón a la transacción.
+ * @param reason Razón
+ * */
 void Transaction::setReason(string reason) {
     this->reason = reason;
 }
 
+/**
+ * Obtiene el tipo de la transacción.
+ * @return tipo
+ * */
 TransactionType Transaction::getType() {
     return this->type;
 }
 
+/**
+ * Asigna el tipo de transacción a la transacción.
+ * @param type Tipo de transacción
+ * */
 void Transaction::setType(TransactionType type) {
     this->type = type;
 }
